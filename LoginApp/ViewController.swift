1fd0cda1479cef714c2b0f7a6f8975fe6f581bf7
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-06-20 00:40:33 -0700

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Arseniy Oksenoyt on 6/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
